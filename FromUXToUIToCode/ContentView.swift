cf0a4bb8b56af1aabfe3e57037841bcba550c844
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-09-27 19:08:41 -0300

    Initial Commit

---
+++ FromUXToUIToCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FromUXToUIToCode
+//
+//  Created by Raphael Cerqueira on 27/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FromUXToUIToCode/FromUXToUIToCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FromUXToUIToCodeApp.swift
+//  FromUXToUIToCode
+//
+//  Created by Raphael Cerqueira on 27/09/20.
+//
+
+import SwiftUI
+
+@main
+struct FromUXToUIToCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
